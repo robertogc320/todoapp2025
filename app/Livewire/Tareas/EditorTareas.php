a128de9a87d27e7d7f14513a339e465e52981d13
robertogc320 <robertogc320@gmail.com>
2025-01-22 23:47:14 -0600

    add livewire EditorTareas

---
+++ app/Livewire/Tareas/EditorTareas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Tareas;
+
+use Livewire\Component;
+
+class EditorTareas extends Component
+{
+    public function render()
+    {
+        return view('livewire.tareas.editor-tareas');
+    }
+}
